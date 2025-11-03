7fe3d34dbc6a3ce69ccfb46ca119ae5add3e3da4
Prachwal <prachwal@example.com>
2025-11-03 07:21:52 +0100

    Refactor component exports to use wildcard syntax for cleaner index file

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,28 +1,16 @@
 // Component exports
-export { Alert } from './Alert';
-export { Badge } from './Badge';
-export { Avatar } from './Avatar';
-export { Spinner } from './Spinner';
-export { Divider } from './Divider';
-export { Chip } from './Chip';
-export { Progress } from './Progress';
-export { Skeleton } from './Skeleton';
-export { Card, CardHeader, CardMedia, CardBody, CardFooter } from './Card';
-export { Accordion, AccordionItem } from './Accordion';
-export { Modal } from './Modal';
+export * from './Alert';
+export * from './Badge';
+export * from './Avatar';
+export * from './Spinner';
+export * from './Divider';
+export * from './Chip';
+export * from './Progress';
+export * from './Skeleton';
+export * from './Card';
+export * from './Accordion';
+export * from './Modal';
 
-// Type exports
-export type { AlertProps, AlertVariant } from './Alert';
-export type { BadgeProps, BadgeVariant, BadgeSize } from './Badge';
-export type { AvatarProps, AvatarSize, AvatarShape } from './Avatar';
-export type { SpinnerProps, SpinnerSize, SpinnerVariant } from './Spinner';
-export type { DividerProps, DividerOrientation, DividerStyle } from './Divider';
-export type { ChipProps, ChipVariant, ChipColor, ChipSize } from './Chip';
-export type { ProgressProps, ProgressVariant, ProgressSize, ProgressColor } from './Progress';
-export type { SkeletonProps, SkeletonVariant, SkeletonAnimation } from './Skeleton';
-export type { CardProps, CardVariant } from './Card';
-export type { AccordionProps, AccordionItemProps } from './Accordion';
-export type { ModalProps, ModalSize } from './Modal';
 export * from './Button';
 export * from './Input';
 export * from './Textarea';
